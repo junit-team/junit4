5a274eab9d8434c70adfc57695d3bbfeceddfa91
emeade <emeade>
2003-12-20 20:22:24 +0000

    fix doc

--- junit/framework/TestCase.java
+++ junit/framework/TestCase.java
@@ -49,7 +49,7 @@ import java.lang.reflect.*;
  * In this case the name of the test case has to correspond to the test method
  * to be run.
  * <pre>
- * TestCase= new MathTest("testAdd");
+ * TestCase test = new MathTest("testAdd");
  * test.run();
  * </pre>
  * The tests to be run can be collected into a TestSuite. JUnit provides
