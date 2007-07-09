97cc9acfa0c81cc440eb8c75c39d590f345b362b
dsaff <dsaff>
2007-07-09 20:53:58 +0000

    Fixed build.xml after adding Theories

--- org/junit/experimental/Invariant.java
+++
@@ -1,5 +0,0 @@
-package org.junit.experimental;
-
-public @interface Invariant {
-
-}
--- src/junit/runner/Version.java
+++ src/junit/runner/Version.java
@@ -9,7 +9,7 @@ public class Version {
 	}
 
 	public static String id() {
-		return "4.4-snapshot-20070629-0646";
+		return "4.4-snapshot-20070709-1651";
 	}
 	
 	public static void main(String[] args) {
