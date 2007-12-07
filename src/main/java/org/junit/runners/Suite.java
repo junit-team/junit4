1796fa2ddd8ab6e2551d40b1b1a63cd954837b19
dsaff <dsaff>
2007-12-07 17:45:07 +0000

    Suite is again a CompositeRunner

--- src/main/java/org/junit/runners/Suite.java
+++ src/main/java/org/junit/runners/Suite.java
@@ -9,17 +9,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.junit.internal.runners.CompositeRunner;
 import org.junit.internal.runners.InitializationError;
-import org.junit.internal.runners.ParentRunner;
-import org.junit.runner.Description;
 import org.junit.runner.Request;
 import org.junit.runner.Runner;
-import org.junit.runner.manipulation.Filter;
-import org.junit.runner.manipulation.Filterable;
-import org.junit.runner.manipulation.NoTestsRemainException;
-import org.junit.runner.manipulation.Sortable;
-import org.junit.runner.manipulation.Sorter;
-import org.junit.runner.notification.RunNotifier;
 
 /**
  * Using <code>Suite</code> as a runner allows you to manually
@@ -28,7 +21,7 @@ import org.junit.runner.notification.RunNotifier;
  * with <code>@RunWith(Suite.class)</code> and <code>@SuiteClasses(TestClass1.class, ...)</code>.
  * When you run this class, it will run all the tests in all the suite classes.
  */
-public class Suite extends ParentRunner<Runner> implements Filterable, Sortable {
+public class Suite extends CompositeRunner {
 	/**
 	 * The <code>SuiteClasses</code> annotation specifies the classes to be run when a class
 	 * annotated with <code>@RunWith(Suite.class)</code> is run.
@@ -49,10 +42,9 @@ public class Suite extends ParentRunner<Runner> implements Filterable, Sortable 
 	// This won't work correctly in the face of concurrency. For that we need to
 	// add parameters to getRunner(), which would be much more complicated.
 	private static Set<Class<?>> parents = new HashSet<Class<?>>();
-	private List<Runner> fRunners = new ArrayList<Runner>();
 	
 	protected Suite(Class<?> klass, Class<?>[] annotatedClasses) throws InitializationError {
-		super(klass);
+		super(klass, klass.getName());
 		
 		addParent(klass);
 		for (Class<?> each : annotatedClasses) {
@@ -65,11 +57,6 @@ public class Suite extends ParentRunner<Runner> implements Filterable, Sortable 
 		List<Throwable> errors= new ArrayList<Throwable>();
 		getTestClass().validateStaticMethods(errors);
 		assertValid(errors);
-
-	}
-
-	private void add(Runner childRunner) {
-		fRunners .add(childRunner);
 	}
 
 	private Class<?> addParent(Class<?> parent) throws InitializationError {
@@ -87,47 +74,5 @@ public class Suite extends ParentRunner<Runner> implements Filterable, Sortable 
 		if (annotation == null)
 			throw new InitializationError(String.format("class '%s' must have a SuiteClasses annotation", klass.getName()));
 		return annotation.value();
-	}
-	
-	protected void validate(List<Throwable> errors) {
-		getTestClass().validateStaticMethods(errors);
-		getTestClass().validateInstanceMethods(errors);
-	}
-
-	@Override
-	public List<Runner> getChildren() {
-		return fRunners;
-	}
-
-	@Override
-	protected void runChild(Runner each, final RunNotifier notifier) {
-		each.run(notifier);
-	}
-
-	@Override
-	protected Description describeChild(Runner runner) {
-		return runner.getDescription();
-	}
-
-	// TODO: (Dec 1, 2007 11:37:46 PM) absorb into parent?
-	
-	// TODO: (Dec 7, 2007 9:40:11 AM) DUP with CompositeRunner
-
-	@Override
-	protected Runner filterChild(Runner child, Filter filter) throws NoTestsRemainException {
-		// TODO: (Dec 7, 2007 9:38:08 AM) shouldn't have this check here
-
-		if (filter != null)
-			filter.apply(child);		
-		return child;
-	}
-	
-	@Override
-	protected Runner sortChild(Runner child, Sorter sorter) {
-		// TODO: (Dec 7, 2007 9:38:57 AM) shouldn't have this check here, and deal with duplication throughout
-
-		if (sorter != null)
-			sorter.apply(child);
-		return child;
 	}
 }
