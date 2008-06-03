3a7fe9fb7714a890a307ff6d7695e70548ebc457
dsaff <dsaff>
2008-06-03 16:16:52 +0000

    Added profile task to build.xml

--- src/main/java/junit/runner/Version.java
+++ src/main/java/junit/runner/Version.java
@@ -9,7 +9,7 @@ public class Version {
 	}
 
 	public static String id() {
-		return "4.5-SNAPSHOT-20080513-1059";
+		return "4.5-SNAPSHOT-20080602-1631";
 	}
 	
 	public static void main(String[] args) {
