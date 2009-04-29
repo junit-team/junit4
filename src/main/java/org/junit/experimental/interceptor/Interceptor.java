28d40d93420cacdb4d420b04475bafdc7d89d922
David Saff <saff@new-host-4.home>
2009-04-28 23:29:50 -0400

    Interceptors and Watchmen, because we're just that cool.

---
+++ src/main/java/org/junit/experimental/interceptor/Interceptor.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.junit.experimental.interceptor;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Interceptor {
+
+}
---
+++ src/main/java/org/junit/experimental/interceptor/Interceptors.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.junit.experimental.interceptor;
+
+import java.lang.reflect.Field;
+
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.Statement;
+
+public class Interceptors extends BlockJUnit4ClassRunner {
+	public Interceptors(Class<?> klass) throws InitializationError {
+		super(klass);
+	}
+
+	@Override
+	protected Statement withAfters(FrameworkMethod method, Object target,
+			Statement statement) {
+		// TODO Auto-generated method stub
+		// TODO (Apr 28, 2009 10:55:21 PM): parameter clump?
+		return super.withAfters(method, target, intercept(statement, target, method));
+	}
+
+	private Statement intercept(Statement statement, Object target, FrameworkMethod method) {
+		Class<?> javaClass= getTestClass().getJavaClass();
+		Field[] fields= javaClass.getFields();
+		Statement result= statement;
+		for (Field each : fields) {
+			if (each.getAnnotation(Interceptor.class) != null) {
+				try {
+					StatementInterceptor interceptor= (StatementInterceptor) each
+							.get(target);
+					result= interceptor.intercept(result, method);
+				} catch (IllegalAccessException e) {
+					throw new RuntimeException(
+							"How did getFields return a field we couldn't access?");
+				}
+			}
+		}
+		return result;
+	}
+}
