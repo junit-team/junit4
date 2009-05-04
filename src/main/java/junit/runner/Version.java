a24d1ce6ad8b83c58e0ecb1c9a17897d540d5bd5
David Saff <saff@dhcp-172-31-197-71.cam.corp.google.com>
2009-05-04 13:40:44 -0400

    Don't edit Version.java manually.  Really.  I mean it.

--- src/main/java/junit/runner/Version.java
+++ src/main/java/junit/runner/Version.java
@@ -9,7 +9,7 @@ public class Version {
 	}
 
 	public static String id() {
-		return "4.7-SNAPSHOT-20090428-1600";
+		return "4.7-SNAPSHOT-20090507-1339";
 	}
 	
 	public static void main(String[] args) {
