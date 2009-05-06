441ab8503a4f09a670ac6e7686ace1485583164d
David Saff <saff@dhcp-172-31-197-71.cam.corp.google.com>
2009-05-06 13:40:40 -0400

    starting() can throw exception

--- src/main/java/org/junit/experimental/interceptor/TestWatchman.java
+++ src/main/java/org/junit/experimental/interceptor/TestWatchman.java
@@ -31,7 +31,7 @@ public class TestWatchman implements StatementInterceptor {
 	public void failed(Throwable e, FrameworkMethod method) {
 	}
 
-	public void starting(FrameworkMethod method) {
+	public void starting(FrameworkMethod method) throws Exception {
 	}
 
 	public void finished(FrameworkMethod method) {
