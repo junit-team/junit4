b5573b70809bed76a0397bb5bb196b6b2144d3d4
David Saff <saff@dhcp-172-31-197-71.cam.corp.google.com>
2009-05-13 23:23:02 +0800

    ParallelComputer doesn't have to assume BlockJUnit4Runner anymore
    Signed-off-by: Kent Beck <kent@threeriversinstitute.org>

--- src/main/java/org/junit/experimental/ParallelComputer.java
+++ src/main/java/org/junit/experimental/ParallelComputer.java
@@ -9,9 +9,7 @@ import java.util.concurrent.Future;
 
 import org.junit.runner.Computer;
 import org.junit.runner.Runner;
-import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.ParentRunner;
-import org.junit.runners.Suite;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 import org.junit.runners.model.RunnerInterceptor;
@@ -34,47 +32,47 @@ public class ParallelComputer extends Computer {
 		return new ParallelComputer(false, true);
 	}
 
-	private static <T> Runner parallelize(ParentRunner<T> runner) {
-		runner.setRunnerInterceptor(new RunnerInterceptor() {
-			private final List<Future<Object>> fResults= new ArrayList<Future<Object>>();
+	private static <T> Runner parallelize(Runner runner) {
+		if (runner instanceof ParentRunner<?>) {
+			((ParentRunner<?>) runner).setRunnerInterceptor(new RunnerInterceptor() {
+				private final List<Future<Object>> fResults= new ArrayList<Future<Object>>();
 
-			private final ExecutorService fService= Executors
-					.newCachedThreadPool();
+				private final ExecutorService fService= Executors
+						.newCachedThreadPool();
 
-			public void runChild(final Runnable childStatement) {
-				fResults.add(fService.submit(new Callable<Object>() {
-					public Object call() throws Exception {
-						childStatement.run();
-						return null;
-					}
-				}));
-			}
+				public void runChild(final Runnable childStatement) {
+					fResults.add(fService.submit(new Callable<Object>() {
+						public Object call() throws Exception {
+							childStatement.run();
+							return null;
+						}
+					}));
+				}
 
-			public void finished() {
-				for (Future<Object> each : fResults)
-					try {
-						each.get();
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-			}
-		});
+				public void finished() {
+					for (Future<Object> each : fResults)
+						try {
+							each.get();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+				}
+			});
+		}
 		return runner;
 	}
 
 	@Override
 	public Runner getSuite(RunnerBuilder builder, java.lang.Class<?>[] classes)
 			throws InitializationError {
-		Suite suite= (Suite) super.getSuite(builder, classes);
+		Runner suite= super.getSuite(builder, classes);
 		return fClasses ? parallelize(suite) : suite;
 	}
 
 	@Override
 	protected Runner getRunner(RunnerBuilder builder, Class<?> testClass)
 			throws Throwable {
-		// TODO (May 4, 2009 4:09:16 PM): no guarantees here
-		BlockJUnit4ClassRunner runner= (BlockJUnit4ClassRunner) super
-				.getRunner(builder, testClass);
+		Runner runner= super.getRunner(builder, testClass);
 		return fMethods ? parallelize(runner) : runner;
 	}
 }
