a5aa7cf7d61162947504060c9d3b6d4db6c073cf
David Saff <saff@dhcp-172-31-197-71.cam.corp.google.com>
2009-05-29 23:20:30 +0800

    I think this is the end of hamcrest generics
    Signed-off-by: Kent Beck <kent@threeriversinstitute.org>

--- src/main/java/org/junit/matchers/JUnitMatchers.java
+++ src/main/java/org/junit/matchers/JUnitMatchers.java
@@ -1,5 +1,11 @@
 package org.junit.matchers;
 
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matcher;
 import org.hamcrest.core.CombinableMatcher;
@@ -85,11 +91,29 @@ public class JUnitMatchers {
 	/**
 	 * This is useful for fluently combining matchers where either may pass, for example:
 	 * <pre>
-	 *   assertThat(string, both(containsString("a")).and(containsString("b")));
+	 *   assertThat(string, either(containsString("a")).or(containsString("b")));
 	 * </pre>
+	 * 
+	 * If you want to say either(is(3)).or(is(4)), and are prevented,
+	 * please see isOneOf(...) below.
 	 */
 	public static <T> CombinableMatcher<T> either(Matcher<T> matcher) {
 		return CoreMatchers.either(matcher);
+	}
+	
+	/**
+	 * This is sugar for the situation where 
+	 * For example:
+	 * <pre>
+	 *   assertThat(string, eitherIs("a").or(is("b")));
+	 * </pre>
+	 */
+	public static <T> Matcher<T> isOneOf(T... objects) {
+		List<Matcher<? super T>> matchers = new ArrayList<Matcher<? super T>>();
+		for (T each : objects) {
+			matchers.add(is(each));
+		}
+		return anyOf(matchers);
 	}
 	
 	/**
@@ -101,9 +125,11 @@ public class JUnitMatchers {
 	 *   assertThat(3, matches(containsString("a")));
 	 * </pre>
 	 * 
-	 * Real example due to unfortunate "both" typing:
+	 * Real example:
 	 * <pre>
-	 *   assertThat(3, either(is(3)).or(matches(is(4))));
+	 *   assertThat(3, either(matches(is(String.class))).or(
+	 *		                  matches(is(Integer.class))));
+     *
 	 * </pre>
 	 */
 	@SuppressWarnings("unchecked")
