50b77b1f3108aa77af0906162d552e513936c322
David Saff <saff@redredrobot.local>
2009-06-15 22:12:10 -0700

    Advance version, and beef up ErrorCollector with checkThat and checkPasses

--- src/main/java/junit/runner/Version.java
+++ src/main/java/junit/runner/Version.java
@@ -9,7 +9,7 @@ public class Version {
 	}
 
 	public static String id() {
-		return "4.7-SNAPSHOT-20090601-1258";
+		return "4.7-SNAPSHOT-20090604-0055";
 	}
 	
 	public static void main(String[] args) {
--- src/main/java/org/junit/experimental/interceptor/ErrorCollector.java
+++ src/main/java/org/junit/experimental/interceptor/ErrorCollector.java
@@ -3,13 +3,16 @@
  */
 package org.junit.experimental.interceptor;
 
+import static org.junit.Assert.assertThat;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
+import org.hamcrest.Matcher;
 import org.junit.internal.runners.model.MultipleFailureException;
 
 public class ErrorCollector extends Verifier {
-
 	private List<Throwable> errors= new ArrayList<Throwable>();
 
 	@Override
@@ -21,4 +24,22 @@ public class ErrorCollector extends Verifier {
 		errors.add(error);
 	}
 
+	// TODO (Jun 15, 2009 10:00:10 PM): is this same as assertThat?
+	public <T> void checkThat(T value, Matcher<T> matcher) {
+		// TODO (Jun 15, 2009 10:01:28 PM): checkReturns
+		try {
+			assertThat(value, matcher);
+		} catch (AssertionError e) {
+			addError(e);
+		}
+	}
+
+	public Object checkSucceeds(Callable<Object> callable) {
+		try {
+			return callable.call();
+		} catch (Throwable e) {
+			addError(e);
+			return null;
+		}		
+	}
 }
