5501ad5d546427b228b208da15cec997e0b13cc8
David Saff <saff@redredrobot.local>
2009-07-02 17:24:29 -0400

    Finished Max javadoc

--- src/main/java/org/junit/experimental/max/MaxCore.java
+++ src/main/java/org/junit/experimental/max/MaxCore.java
@@ -18,36 +18,70 @@ import org.junit.runner.Runner;
 import org.junit.runners.Suite;
 import org.junit.runners.model.InitializationError;
 
+/**
+ * A replacement for JUnitCore, which keeps track of runtime and failure history, and reorders tests
+ * to maximize the chances that a failing test occurs early in the test run.
+ * 
+ * The rules for sorting are:
+ * <ol>
+ * <li> Never-run tests first, in arbitrary order
+ * <li> Group remaining tests by the date at which they most recently failed.
+ * <li> Sort groups such that the most recent failure date is first, and never-failing tests are at the end.
+ * <li> Within a group, run the fastest tests first. 
+ * </ol>
+ */
 public class MaxCore {
 	private static final String MALFORMED_JUNIT_3_TEST_CLASS_PREFIX= "malformed JUnit 3 test class: ";
-
-	public static MaxCore forFolder(String fileName) {
-		return storedLocally(new File(fileName));
-	}
 	
+	/**
+	 * Create a new MaxCore from a serialized file stored at storedResults
+	 */
 	public static MaxCore storedLocally(File storedResults) {
 		return new MaxCore(storedResults);
 	}
 
-	public final MaxHistory fHistory;
+	private final MaxHistory fHistory;
 
-	public MaxCore(File storedResults) {
-		fHistory = MaxHistory.forFolder(storedResults);
+	private MaxCore(File storedResults) {
+		fHistory = MaxHistory.locallyStored(storedResults);
 	}
 
+	/**
+	 * Run all the tests in <code>class</code>.
+	 * @return a {@link Result} describing the details of the test run and the failed tests.
+	 */
 	public Result run(Class<?> testClass) {
 		return run(Request.aClass(testClass));
 	}
 
+	/**
+	 * Run all the tests contained in <code>request</code>.
+	 * @param request the request describing tests
+	 * @return a {@link Result} describing the details of the test run and the failed tests.
+	 */
 	public Result run(Request request) {
 		return run(request, new JUnitCore());
 	}
 
+	/**
+	 * Run all the tests contained in <code>request</code>.
+	 * 
+	 * This variant should be used if {@code core} has attached listeners that this
+	 * run should notify.
+	 * 
+	 * @param request the request describing tests
+	 * @param core a JUnitCore to delegate to.
+	 * @return a {@link Result} describing the details of the test run and the failed tests.
+	 */
 	public Result run(Request request, JUnitCore core) {
 		core.addListener(fHistory.listener());
 		return core.run(sortRequest(request).getRunner());
 	}
 	
+	/**
+	 * @param request
+	 * @return a new Request, which contains all of the same tests, but in a new order.
+	 */
 	public Request sortRequest(Request request) {
 		if (request instanceof SortingRequest) // We'll pay big karma points for this
 			return request;
@@ -98,6 +132,11 @@ public class MaxCore {
 		}
 	}
 
+	/**
+	 * @param request a request to run
+	 * @return a list of method-level tests to run, sorted in the order
+	 * specified in the class comment.
+	 */
 	public List<Description> sortedLeavesForTest(Request request) {
 		return findLeaves(sortRequest(request));
 	}
