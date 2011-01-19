3c7c3b78065f1fc85f655bb60b3dad4ed33854a0
Kevin Cooney <kcooney@google.com>
2011-01-18 20:54:52 -0800

    Give InitializationError a useful message. JUnit itself does not use the message, but having a useful message makes debugging runners easier.

--- src/main/java/org/junit/runners/model/InitializationError.java
+++ src/main/java/org/junit/runners/model/InitializationError.java
@@ -36,4 +36,14 @@ public class InitializationError extends Exception {
 	public List<Throwable> getCauses() {
 		return fErrors;
 	}
+
+	@Override
+	public String getMessage() {
+		StringBuilder sb = new StringBuilder(
+				String.format("There were %d errors:", fErrors.size()));
+		for (Throwable e : fErrors) {
+			sb.append(String.format("\n  %s(%s)", e.getClass().getName(), e.getMessage()));
+		}
+		return sb.toString();
+	}
 }
