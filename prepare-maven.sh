0fa9d62b6bc1f1be73e0e1b1e78d65d806f5c9ce
Ryan Breidenbach <ryan.breidenbach@gmail.com>
2011-03-08 22:26:59 -0600

    Close to feature complete for the first pass: - Creates complete bundles ready for central repo uploading - Integration with Ant, including secure passphrase prompting

--- prepare-maven.sh
+++ prepare-maven.sh
@@ -1,10 +1,15 @@
+# Configure variables
 if [ "$1" == "" ]; then
-    echo "Usage: prepare-maven.sh <JUNIT_VERSION>"
+    echo "Usage: prepare-maven.sh JUNIT_VERSION <PASSPHRASE>"
 	exit
 fi
-
-# Configure variables
 VERSION=$1
+
+PASSPHRASE=$2
+if [ "$PASSPHRASE" == "" ]; then
+    read -s -p "Enter passphrase: " PASSPHRASE
+fi
+
 DIST_DIR=junit$VERSION
 JUNIT_DIR=target/maven/junit-$VERSION
 JUNIT_DEP_DIR=target/maven/junit-dep-$VERSION
@@ -30,11 +35,14 @@ jar -cf $JUNIT_DEP_DIR/junit-$VERSION-javadoc.jar -C $DIST_DIR/javadoc .
 jar -cf $JUNIT_DIR/junit-$VERSION-sources.jar -C $DIST_DIR/org . -C $DIST_DIR/junit . -C $DIST_DIR/temp.hamcrest.source .
 jar -cf $JUNIT_DEP_DIR/junit-dep-$VERSION-sources.jar -C $DIST_DIR/org . -C $DIST_DIR/junit .
 
-#S ign files
-read -s -p "Enter passphrase: " PASSPHRASE
+#Sign files
 find $JUNIT_DIR -type f -exec gpg -ab --passphrase "$PASSPHRASE" {} \;
 find $JUNIT_DEP_DIR -type f -exec gpg -ab --passphrase "$PASSPHRASE" {} \;
 
 # Rename POM sig file to expeted convention
 mv $JUNIT_DIR/pom.xml.asc $JUNIT_DIR/junit-$VERSION.pom.asc
 mv $JUNIT_DEP_DIR/pom.xml.asc $JUNIT_DEP_DIR/junit-dep-$VERSION.pom.asc
+
+# Creat bundled JARs
+jar -Mcf $DIST_DIR/junit-$VERSION-bundle.jar -C $JUNIT_DIR .
+jar -Mcf $DIST_DIR/junit-dep-$VERSION-bundle.jar -C $JUNIT_DEP_DIR .
