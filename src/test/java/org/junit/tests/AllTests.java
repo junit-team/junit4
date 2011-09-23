faba199e389e3c977fc5573315790a5cde5f7611
Matthew Farwell <matthew@farwell.co.uk>
2011-09-23 20:52:33 +0200

    This is a fix for #83 support @Rule on methods Extend rules to support @Rule public MethodRule someRule() { return new SomeRule(); }
    Scala does not allow the creation of public fields. If you declare a field public, the field is created as private but with accessor methods.
    To allow the use of @Rule with Scala fields, this patch allows the addition
    of @Rule to methods. When BlockJUnit4ClassRunner retrieves the list of fields
    that have the @Rule annotation, it also searches for methods with @Rule.
    These methods are called and the values returned are added to the list of
    TestRule.
    
    @ClassRule has been similarly changed.
    
    This is intended to be used as follows:
    
    private ExpectedException thrown = rules.ExpectedException.none();
    
    @Rule public ExpectedException getThrown() {
        return thrown;
    }
    
    or
    
    @Rule public Timeout getTimeout() {
        return new Timeout(20);
    }
    
    The validation is exactly the same as for the fields, i.e the methods must be
    public and return a TestRule/MethodRule. In the case of @ClassRule, the
    methods must also be static.

--- src/test/java/org/junit/tests/AllTests.java
+++ src/test/java/org/junit/tests/AllTests.java
@@ -29,6 +29,7 @@ import org.junit.tests.experimental.rules.ExpectedExceptionRuleTest;
 import org.junit.tests.experimental.rules.ExternalResourceRuleTest;
 import org.junit.tests.experimental.rules.MethodRulesTest;
 import org.junit.tests.experimental.rules.NameRulesTest;
+import org.junit.tests.experimental.rules.RuleFieldValidatorTest;
 import org.junit.tests.experimental.rules.RuleChainTest;
 import org.junit.tests.experimental.rules.TempFolderRuleTest;
 import org.junit.tests.experimental.rules.TestRuleTest;
@@ -77,7 +78,7 @@ import org.junit.tests.validation.FailedConstructionTest;
 import org.junit.tests.validation.InaccessibleBaseClassTest;
 import org.junit.tests.validation.ValidationTest;
 
-// These test files need to be cleaned.  See
+// These test files need to be cleaned. See
 // https://sourceforge.net/pm/task.php?func=detailtask&project_task_id=136507&group_id=15278&group_project_id=51407
 
 @SuppressWarnings("deprecation")
@@ -152,9 +153,10 @@ import org.junit.tests.validation.ValidationTest;
 	CategoryTest.class,
 	CategoriesAndParameterizedTest.class,
 	ParentRunnerFilteringTest.class,
+	BlockJUnit4ClassRunnerOverrideTest.class,
+	RuleFieldValidatorTest.class,
 	RuleChainTest.class,
-	BlockJUnit4ClassRunnerTest.class,
-	BlockJUnit4ClassRunnerOverrideTest.class
+	BlockJUnit4ClassRunnerTest.class
 })
 public class AllTests {
 	public static Test suite() {
