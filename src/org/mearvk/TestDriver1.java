681d1e0448db084a523f67de64255a605a3fecf6
unknown <Max Rupplin@HomeUser-PC.(none)>
2012-02-23 18:18:38 -0500

    Working, untested impl

---
+++ src/org/mearvk/TestDriver1.java
@@ -0,0 +1,33 @@
+package org.mearvk;
+
+import java.util.List;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestDriver1
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			JUnitCore core = new JUnitCore();
+			
+			//Result result = core.run(new TestOrderingComputer(), MyTestClass1.class, MyTestClass2.class);
+
+            Result result = core.run(MyTestClass1.class, MyTestClass2.class, MyTestClass3.class);
+			
+			List<Failure> failures = result.getFailures();
+			
+			for(Failure failure : failures)
+			{
+				System.err.println("Failure messages: "+failure.getMessage());
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
