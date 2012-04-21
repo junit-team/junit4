1df71004338faf30b9287b03b726e1ce4f1246be
Marc Philipp <mail@marcphilipp.de>
2012-04-21 21:56:51 +0200

    Fix #88

---
+++ src/test/java/org/junit/tests/experimental/theories/TestedOnSupplierTest.java
@@ -0,0 +1,32 @@
+package org.junit.tests.experimental.theories;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.experimental.theories.ParameterSignature;
+import org.junit.experimental.theories.PotentialAssignment;
+import org.junit.experimental.theories.suppliers.TestedOn;
+import org.junit.experimental.theories.suppliers.TestedOnSupplier;
+
+public class TestedOnSupplierTest {
+
+	public void foo(@TestedOn(ints= { 1 }) int x) {
+	}
+
+	@Test
+	public void descriptionStatesParameterName() throws Exception {
+		TestedOnSupplier supplier= new TestedOnSupplier();
+		List<PotentialAssignment> assignments= supplier.getValueSources(signatureOfFoo());
+		assertThat(assignments.get(0).getDescription(), is("ints"));
+	}
+
+	private ParameterSignature signatureOfFoo() throws NoSuchMethodException {
+		Method method= getClass().getMethod("foo", int.class);
+		return ParameterSignature.signatures(method).get(0);
+	}
+
+}
