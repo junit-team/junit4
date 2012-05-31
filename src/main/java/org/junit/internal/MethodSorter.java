8424e265a3f96bebb2ee4dbced7c3e3e128f91f5
Ian Beauregard <ian.beauregard@gmail.com>
2012-05-31 09:09:24 -0400

    changes as suggested by kcooney

--- src/main/java/org/junit/internal/MethodSorter.java
+++ src/main/java/org/junit/internal/MethodSorter.java
@@ -22,14 +22,13 @@ public class MethodSorter {
     };
     
     /**
-     * Method name ascending lexicographic sort order, with toString as a tiebreaker
+     * Method name ascending lexicographic sort order, with {@link Method#toString()} as a tiebreaker
      */
     public static Comparator<Method> NAME_ASCENDING= new Comparator<Method>() {
         public int compare(Method m1, Method m2) {
             final int comparison = m1.getName().compareTo(m2.getName());
-            if (comparison != 0) {
+            if (comparison != 0)
                 return comparison;
-            }
             return m1.toString().compareTo(m2.toString());
         }
     };
--- src/main/java/org/junit/runners/MethodSorters.java
+++ src/main/java/org/junit/runners/MethodSorters.java
@@ -10,11 +10,16 @@ import org.junit.internal.MethodSorter;
  * Defines common {@link MethodSorter} implementations.
  */
 public enum MethodSorters {
-    /** Sorts the test methods by the method name, in lexicographic order, with Method.toString used as a tiebreaker */
+    /** Sorts the test methods by the method name, in lexicographic order,
+     * with {@link Method#toString()} used as a tiebreaker
+     */
     NAME_ASCENDING(MethodSorter.NAME_ASCENDING),
+
     /** Leaves the test methods in the order returned by the JVM.
-     * Note that the order from the JVM my vary from run to run */
+     * Note that the order from the JVM may vary from run to run
+     */
     JVM(null),
+
     /** Sorts the test methods in a deterministic, but not predictable, order */
     DEFAULT(MethodSorter.DEFAULT);
     
