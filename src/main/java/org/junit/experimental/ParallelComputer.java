7898dd861deabd8afca60614f4bb546828c28d70
Tibor Digana <tibor17@lycos.com>
2012-09-22 01:32:30 +0200

    import

--- src/main/java/org/junit/experimental/ParallelComputer.java
+++ src/main/java/org/junit/experimental/ParallelComputer.java
@@ -1,11 +1,8 @@
 package org.junit.experimental;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.runner.Computer;
 import org.junit.runner.Runner;
