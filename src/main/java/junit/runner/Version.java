32b5270071e08297f167a1785fd71b92ab6d7c1b
Tibor Digana <tibor17@lycos.com>
2012-09-23 00:56:15 +0200

    Maven project junit:junit artifact

--- src/main/java/junit/runner/Version.java
+++ src/main/java/junit/runner/Version.java
@@ -1,18 +1,41 @@
 package junit.runner;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * This class defines the current version of JUnit
  */
 public class Version {
+    private static final String VERSION;
+    static {
+        try {
+            final Properties properties= new Properties();
+            InputStream pomProps= getPomPropertiesAsStream("junit");
+            if (pomProps == null) pomProps= getPomPropertiesAsStream("junit-dep");
+            if (pomProps != null) properties.load(pomProps);
+            VERSION= properties.getProperty("version", "<version>");
+            if (pomProps != null) pomProps.close();
+            assert !VERSION.equals("<version>") : "your class loader does not load resources at /META-INF/maven/junit/...";
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
 	private Version() {
 		// don't instantiate
 	}
 
 	public static String id() {
-		return "4.11-SNAPSHOT";
+		return VERSION;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println(id());
 	}
+
+    private static InputStream getPomPropertiesAsStream(String artifactId) {
+        return Version.class.getResourceAsStream("/META-INF/maven/junit/" + artifactId + "/pom.properties");
+    }
 }
