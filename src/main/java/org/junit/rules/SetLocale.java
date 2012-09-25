e74e59e9f8cd9b28d8cb2381c3363160c45ecbb5
David Grant <david@grant.org.uk>
2012-09-25 08:54:57 +0100

    Added SetLocale example.

--- src/main/java/org/junit/rules/SetLocale.java
+++ src/main/java/org/junit/rules/SetLocale.java
@@ -5,13 +5,42 @@ import java.util.Locale;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+/**
+ * The SetLocale rule allows setting of a Locale for the duration
+ * of a test.
+ * 
+ * <pre>
+ * public static class UsesSetLocale {
+ *   &#064;Rule
+ *   SetLocale setLocale = new SetLocale(Locale.FRANCE);
+ *   
+ *   &#064;Test
+ *   public void testExample() {
+ *     Calendar cal= GregorianCalendar.getInstance();
+ *     cal.set(2000, 0, 1);
+ *
+ *     Assert.assertEquals("Month does not match", "janvier",
+ *       cal.getDisplayName(MONTH, LONG, Locale.getDefault()));
+ *   }
+ * }
+ * </pre>
+ */
 public class SetLocale implements TestRule {
 	private final Locale locale;
 	
+	/**
+	 * Creates a new instance of {@code SetLocale} with the provided 
+	 * {@link Locale}.
+	 * 
+	 * @param locale the locale to set.
+	 */
 	public SetLocale(Locale locale) {
 		this.locale = locale;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public Statement apply(Statement base, Description description) {
 		return statement(base);
 	}
