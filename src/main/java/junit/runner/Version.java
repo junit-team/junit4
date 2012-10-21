0d508269ec972c3ac22e265bc0d9d8305f2afb8b
Tibor Digana <tibor17@lycos.com>
2012-10-21 23:57:12 +0200

    fixed indentation in Version.java

--- src/main/java/junit/runner/Version.java
+++ src/main/java/junit/runner/Version.java
@@ -22,21 +22,21 @@ public class Version {
 		System.out.println(id());
 	}
 
-    private static InputStream getPomPropertiesAsStream(String artifactId) {
-        return Version.class.getResourceAsStream("/META-INF/maven/junit/" + artifactId + "/pom.properties");
-    }
+	private static InputStream getPomPropertiesAsStream(String artifactId) {
+		return Version.class.getResourceAsStream("/META-INF/maven/junit/" + artifactId + "/pom.properties");
+	}
     
 	private static String getVersion() {
-        try {
-            final Properties properties= new Properties();
-            InputStream pomProps= getPomPropertiesAsStream("junit");
-            if (pomProps != null) properties.load(pomProps);
-            String version= properties.getProperty("version", "<version>");
-            if (pomProps != null) pomProps.close();
-            assert !version.equals("<version>") : "your class loader does not load resources at /META-INF/maven/junit/...";
+		try {
+			final Properties properties= new Properties();
+			InputStream pomProps= getPomPropertiesAsStream("junit");
+			if (pomProps != null) properties.load(pomProps);
+			String version= properties.getProperty("version", "<version>");
+			if (pomProps != null) pomProps.close();
+			assert !version.equals("<version>") : "your class loader does not load resources at /META-INF/maven/junit/...";
 			return version;
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
     }
 }
