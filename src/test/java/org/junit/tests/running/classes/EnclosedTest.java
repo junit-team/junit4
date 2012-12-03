afbeeb315f4a2b2d4b21bf6f38cef2b83df6b97a
Nicolas A. Bérard-Nault <nicobn@gmail.com>
2012-12-03 11:05:03 -0500

    Fixed style (annotations on a separate line)

--- src/test/java/org/junit/tests/running/classes/EnclosedTest.java
+++ src/test/java/org/junit/tests/running/classes/EnclosedTest.java
@@ -14,30 +14,41 @@ public class EnclosedTest {
     @RunWith(Enclosed.class)
     public static class Enclosing {
         public static class A {
-            @Test public void a() {}
-            @Test public void b() {}
+            @Test
+            public void a() {}
+
+            @Test
+            public void b() {}
         }
         public static class B {
-            @Test public void a() {}
-            @Test public void b() {}
-            @Test public void c() {}
+            @Test
+            public void a() {}
+
+            @Test
+            public void b() {}
+
+            @Test
+            public void c() {}
         }
         abstract public static class C {
             @Test public void a() {}
         }
     }
-    
-    @Test public void enclosedRunnerPlansConcreteEnclosedClasses() throws Exception {
+
+    @Test
+    public void enclosedRunnerPlansConcreteEnclosedClasses() throws Exception {
         Runner runner= Request.aClass(Enclosing.class).getRunner();
         assertEquals(5, runner.testCount());
     }
-    
-    @Test public void enclosedRunnerRunsConcreteEnclosedClasses() throws Exception {
+
+    @Test
+    public void enclosedRunnerRunsConcreteEnclosedClasses() throws Exception {
         Result result= JUnitCore.runClasses(Enclosing.class);
         assertEquals(5, result.getRunCount());
     }
-    
-    @Test public void enclosedRunnerIsNamedForEnclosingClass() throws Exception {
+
+    @Test
+    public void enclosedRunnerIsNamedForEnclosingClass() throws Exception {
         assertEquals(Enclosing.class.getName(), Request.aClass(Enclosing.class)
                 .getRunner().getDescription().getDisplayName());
     }
