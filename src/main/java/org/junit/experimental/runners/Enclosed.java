8675e5bf75f4865f3ff2d0f840d0d6022f1c59b6
Nicolas A. Bérard-Nault <nicobn@gmail.com>
2012-12-03 10:25:15 -0500

    Expanded tabs to spaces

--- src/main/java/org/junit/experimental/runners/Enclosed.java
+++ src/main/java/org/junit/experimental/runners/Enclosed.java
@@ -16,10 +16,10 @@ import org.junit.runners.model.RunnerBuilder;
  *  <pre>
  *  \@RunWith(Enclosed.class)
  *  public class ListTests {
- *  	...useful shared stuff...
- *  	public static class OneKindOfListTest {...}
- *  	public static class AnotherKind {...}
- *  	abstract public static class Ignored {...}
+ *      ...useful shared stuff...
+ *      public static class OneKindOfListTest {...}
+ *      public static class AnotherKind {...}
+ *      abstract public static class Ignored {...}
  *  }
  *  </pre>
  *  
@@ -27,22 +27,22 @@ import org.junit.runners.model.RunnerBuilder;
  *  
  */
 public class Enclosed extends Suite {
-	/**
-	 * Only called reflectively. Do not use programmatically.
-	 */
-	public Enclosed(Class<?> klass, RunnerBuilder builder) throws Throwable {
-		super(builder, klass, filterAbstractClasses(klass.getClasses()));
-	}
-	
-	private static Class<?>[] filterAbstractClasses(final Class<?>[] classes) {		
-		final List<Class<?>> filteredList= new ArrayList<Class<?>>(classes.length);
+    /**
+     * Only called reflectively. Do not use programmatically.
+     */
+    public Enclosed(Class<?> klass, RunnerBuilder builder) throws Throwable {
+        super(builder, klass, filterAbstractClasses(klass.getClasses()));
+    }
+    
+    private static Class<?>[] filterAbstractClasses(final Class<?>[] classes) {     
+        final List<Class<?>> filteredList= new ArrayList<Class<?>>(classes.length);
 
-		for (final Class<?> clazz : classes) {
-			if (!Modifier.isAbstract(clazz.getModifiers())) {
-				filteredList.add(clazz);
-			}
-		}
-		
-		return filteredList.toArray(new Class<?>[filteredList.size()]);
-	}	
+        for (final Class<?> clazz : classes) {
+            if (!Modifier.isAbstract(clazz.getModifiers())) {
+                filteredList.add(clazz);
+            }
+        }
+        
+        return filteredList.toArray(new Class<?>[filteredList.size()]);
+    }   
 }
