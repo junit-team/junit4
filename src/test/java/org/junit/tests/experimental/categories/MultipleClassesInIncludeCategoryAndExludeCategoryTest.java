2a434174e9721aafe6ad65fc4d4c2654e9c8d3c2
Henning Gross <igaffai@gmail.com>
2012-12-04 22:48:44 +0100

    Another test for behaviour descriped in JavaDoc

--- src/test/java/org/junit/tests/experimental/categories/MultipleClassesInIncludeCategoryAndExludeCategoryTest.java
+++ src/test/java/org/junit/tests/experimental/categories/MultipleClassesInIncludeCategoryAndExludeCategoryTest.java
@@ -47,6 +47,7 @@ public class MultipleClassesInIncludeCategoryAndExludeCategoryTest {
 
     @Test
     public void testMultipleClassesInIncludeCategory() {
+        // a and b will be ran
         Result result = JUnitCore.runClasses(IncludeCategorySuite.class);
         assertEquals(2, result.getRunCount());
     }
@@ -59,6 +60,21 @@ public class MultipleClassesInIncludeCategoryAndExludeCategoryTest {
 
     @Test
     public void testMultipleClassesInExcludeCategory() {
+        // only c will be ran (not annotated, due to no @IncludeCategory specified)
+        Result result = JUnitCore.runClasses(ExcludeCategorySuite.class);
+        assertEquals(1, result.getRunCount());
+    }
+
+    @RunWith(Categories.class)
+    @Categories.IncludeCategory(SlowTests.class)
+    @Categories.ExcludeCategory({SlowTests.class})
+    @Suite.SuiteClasses({A.class})
+    public static class OverrideIncludeCategorySuite {
+    }
+
+    @Test
+    public void testOverrideCategory() {
+        // only a will be ran (IntegrationTest)
         Result result = JUnitCore.runClasses(ExcludeCategorySuite.class);
         assertEquals(1, result.getRunCount());
     }
