736c9226f2d963b439f38e4ad6074c4ed1dac5df
Tibor Digana <tibor17@lycos.com>
2013-01-04 23:43:10 +0100

    Sorter#NULL seems to be a constant

--- src/main/java/org/junit/runner/manipulation/Sorter.java
+++ src/main/java/org/junit/runner/manipulation/Sorter.java
@@ -14,11 +14,12 @@ public class Sorter implements Comparator<Description> {
     /**
      * NULL is a <code>Sorter</code> that leaves elements in an undefined order
      */
-    public static Sorter NULL = new Sorter(new Comparator<Description>() {
+    public static final Sorter NULL = new Sorter(new Comparator<Description>() {
         public int compare(Description o1, Description o2) {
             return 0;
         }
     });
+
     private final Comparator<Description> fComparator;
 
     /**
