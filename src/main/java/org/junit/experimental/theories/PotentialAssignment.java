39d8b9e93ff544880f39a3497fb428c6cba5288b
pimterry <pimterry@gmail.com>
2013-01-18 00:14:40 +0000

    Improved PotentialAssignment description with better formatting, and handling for failed toString() calls. Refactored tests into proper unit tests closer wrapping the relevant bit of this code too.

--- src/main/java/org/junit/experimental/theories/PotentialAssignment.java
+++ src/main/java/org/junit/experimental/theories/PotentialAssignment.java
@@ -1,5 +1,7 @@
 package org.junit.experimental.theories;
 
+import static java.lang.String.format;
+
 public abstract class PotentialAssignment {
     public static class CouldNotGenerateValueException extends Exception {
         private static final long serialVersionUID = 1L;
@@ -8,22 +10,35 @@ public abstract class PotentialAssignment {
     public static PotentialAssignment forValue(final String name, final Object value) {
         return new PotentialAssignment() {
             @Override
-            public Object getValue() throws CouldNotGenerateValueException {
+            public Object getValue() {
                 return value;
             }
 
             @Override
             public String toString() {
-                return String.format("[%s]", value);
+                return format("[%s]", value);
             }
 
             @Override
-            public String getDescription() throws CouldNotGenerateValueException {
-            	return String.format("%s: %s", name, value);
+            public String getDescription() {
+            	String valueString;
+            	
+            	if (value == null) {
+            		valueString = "null"; 
+            	} else {
+                	try {
+                		valueString = format("\"%s\"", value);
+                	} catch (Throwable e) {
+                		valueString = format("[toString() threw %s: %s]", 
+                							 e.getClass().getSimpleName(), e.getMessage());
+                	}
+            	}
+            	
+            	return format("%s <from %s>", valueString, name);
             }
         };
     }
-
+    
     public abstract Object getValue() throws CouldNotGenerateValueException;
 
     public abstract String getDescription() throws CouldNotGenerateValueException;
