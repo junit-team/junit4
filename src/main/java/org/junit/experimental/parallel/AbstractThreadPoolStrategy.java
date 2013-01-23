ce6c266ebac4f1b20cc188057582833024337b65
Tibor Digana <tibor17@lycos.com>
2013-01-23 21:26:47 +0100

    AbstractThreadPoolStrategy brief javadoc

--- src/main/java/org/junit/experimental/parallel/AbstractThreadPoolStrategy.java
+++ src/main/java/org/junit/experimental/parallel/AbstractThreadPoolStrategy.java
@@ -3,19 +3,11 @@ package org.junit.experimental.parallel;
 import java.util.Collection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 /**
- * Skeletal implementation of {@link SchedulingStrategy} in private package.
- * This is parallel scheduling strategy.
- * <p/>
- * If the strategy shares its own thread pool, the abstract method
- * {@link #hasSharedThreadPool()} should return <code>true</code>.
- * <p/>
- * Strategies which do not share their thread pools with other strategies
- * {@link ExecutorService#awaitTermination(long, TimeUnit) await their termination}
- * in method {@link #awaitStopped()} after the stop request {@link #stop()}.
- * Sharing strategies await only own tasks to be terminated in {@link #awaitStopped()}.
+ * Abstract parallel scheduling strategy in private package.
+ * The remaining abstract methods have to be implemented differently
+ * depending if the thread pool is shared with other strategies or not.
  *
  * @author Tibor Digana (tibor17)
  * @version 4.12
