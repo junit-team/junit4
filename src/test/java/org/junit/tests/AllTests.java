62dc3837bb0de882712885052b389415f0b380c9
Michael Clarke <michael.m.clarke@gmail.com>
2013-01-27 18:22:23 +0000

    Adding unit test for custom Parameterized runner changes

--- src/test/java/org/junit/tests/AllTests.java
+++ src/test/java/org/junit/tests/AllTests.java
@@ -62,6 +62,7 @@ import org.junit.tests.manipulation.SortableTest;
 import org.junit.tests.running.classes.BlockJUnit4ClassRunnerTest;
 import org.junit.tests.running.classes.EnclosedTest;
 import org.junit.tests.running.classes.IgnoreClassTest;
+import org.junit.tests.running.classes.ParameterizedRunWithTest;
 import org.junit.tests.running.classes.ParameterizedTestTest;
 import org.junit.tests.running.classes.ParentRunnerFilteringTest;
 import org.junit.tests.running.classes.ParentRunnerTest;
@@ -104,6 +105,7 @@ import org.junit.tests.validation.ValidationTest;
         ForwardCompatibilityTest.class,
         OldTests.class,
         ParameterizedTestTest.class,
+        ParameterizedRunWithTest.class,
         RunWithTest.class,
         RunnerTest.class,
         SuiteTest.class,
