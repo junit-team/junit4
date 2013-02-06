72af29d7f52e6f3029fe8464c2b0bb63efab2fab
pimterry <pimterry@gmail.com>
2013-02-06 23:50:18 +0000

    Added specific targetting for the various theory annotations

--- src/main/java/org/junit/experimental/theories/Theory.java
+++ src/main/java/org/junit/experimental/theories/Theory.java
@@ -1,9 +1,13 @@
 package org.junit.experimental.theories;
 
+import static java.lang.annotation.ElementType.METHOD;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(METHOD)
 public @interface Theory {
     boolean nullsAccepted() default true;
 }
