114025fce0f28c5d7daa2204c7b91537c62f9f09
Noel Yap <noel.yap+github.com@gmail.com>
2013-03-27 15:41:13 -0700

    public keyword removed from methods of interface.

--- src/main/java/org/junit/runner/FilterFactory.java
+++ src/main/java/org/junit/runner/FilterFactory.java
@@ -12,12 +12,12 @@ public interface FilterFactory {
      * @param params Parameters needed to create the {@link Filter}
      * @throws FilterNotCreatedException
      */
-    public Filter createFilter(FilterFactoryParams params) throws FilterNotCreatedException;
+    Filter createFilter(FilterFactoryParams params) throws FilterNotCreatedException;
 
     /**
      * Exception thrown if the {@link Filter} cannot be created.
      */
-    public static class FilterNotCreatedException extends FilterFactories.FilterFactoryNotCreatedException {
+    static class FilterNotCreatedException extends FilterFactories.FilterFactoryNotCreatedException {
         public FilterNotCreatedException(Exception exception) {
             super(exception);
         }
