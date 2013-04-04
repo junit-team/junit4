8dce52e618f007cc34a99eb74fd83fad0839e49e
Tibor Digana <tibor17@lycos.com>
2013-04-04 12:12:30 +0200

    Generated MANIFEST.MF without Activator. IT tests passed.

--- src/test/java/org/junit/it/osgi/TestActivator.java
+++ src/test/java/org/junit/it/osgi/TestActivator.java
@@ -1,11 +1,11 @@
 package org.junit.it.osgi;
 
+import org.hamcrest.Matcher;
 import org.junit.Assert;
 import org.junit.runner.JUnitCore;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 
 import java.util.ArrayList;
 
@@ -23,6 +23,9 @@ import static org.hamcrest.core.IsSame.sameInstance;
  */
 public final class TestActivator implements BundleActivator {
     public void start(BundleContext bundleContext) throws Exception {
+        // The hamcrest-core:1.3 is not yet OSGi bundle, thus the library has to appear in application ClassLoader.
+        assertThat(Matcher.class.getClassLoader(), is(sameInstance(ClassLoader.getSystemClassLoader())));
+
         String providerBundleSymbolicName = "org.junit";
         String consumerBundleSymbolicName = "org.junit.it.osgi";
         ArrayList<String> symbolicNames = new ArrayList<String>();
@@ -31,17 +34,12 @@ public final class TestActivator implements BundleActivator {
         }
         Assert.assertThat(symbolicNames, hasItems(providerBundleSymbolicName, consumerBundleSymbolicName));
 
-        ServiceReference<JUnitCore> ref = bundleContext.getServiceReference(JUnitCore.class);
-        JUnitCore core = bundleContext.getService(ref);
+        JUnitCore core = new JUnitCore();
 
-        try {
-            assertNotNull(core);
-            assertThat(core.getClass().getClassLoader(), is(not(sameInstance(ClassLoader.getSystemClassLoader()))));
-            assertThat(core.getClass().getClassLoader(), is(not(sameInstance(Thread.currentThread().getContextClassLoader()))));
-            assertNull(core.getClass().getClassLoader().getParent());
-        } finally {
-            bundleContext.ungetService(ref);
-        }
+        assertNotNull(core);
+        assertThat(core.getClass().getClassLoader(), is(not(sameInstance(ClassLoader.getSystemClassLoader()))));
+        assertThat(core.getClass().getClassLoader(), is(not(sameInstance(Thread.currentThread().getContextClassLoader()))));
+        assertNull(core.getClass().getClassLoader().getParent());
     }
 
     public void stop(BundleContext bundleContext) throws Exception {
