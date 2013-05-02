ae1fc256c8a56f0bd4ad88acd1190503002726f7
Noel Yap <noel.yap+github.com@gmail.com>
2013-05-02 09:56:19 -0700

    Javadoc removed. Comments to be created by @kcooney.

--- src/main/java/org/junit/runner/FilterFactoryParams.java
+++ src/main/java/org/junit/runner/FilterFactoryParams.java
@@ -1,24 +1,12 @@
 package org.junit.runner;
 
-/**
- * Parameters to a {@link FilterFactory}.
- */
 public final class FilterFactoryParams {
     private final String args;
 
-    /**
-     * Constructs a {@link FilterFactoryParams}.
-     *
-     */
     public FilterFactoryParams() {
         this("");
     }
 
-    /**
-     * Constructs a {@link FilterFactoryParams}.
-     *
-     * @param args Arguments
-     */
     public FilterFactoryParams(String args) {
         if (args == null) {
             throw new NullPointerException();
@@ -27,9 +15,6 @@ public final class FilterFactoryParams {
         this.args = args;
     }
 
-    /**
-     * Returns the arguments.
-     */
     public String getArgs() {
         return args;
     }
