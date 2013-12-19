967438d490ac741cd188999946b51d6b5b348acf
Robin Stevens <stevensro@gmail.com>
2013-12-19 18:40:50 +0100

    Fixed typo and added link to the RuleChain class

--- src/main/java/org/junit/Rule.java
+++ src/main/java/org/junit/Rule.java
@@ -17,10 +17,11 @@ import java.lang.annotation.Target;
  * then the {@link Test} method, and finally any {@link After} methods,
  * throwing an exception if any of these fail.  If there are multiple
  * annotated {@link Rule}s on a class, they will be applied in order of fields first, then methods.
- * However, if there are mutliple fields (or methods) they will be applied in an order
+ * However, if there are multiple fields (or methods) they will be applied in an order
  * that depends on your JVM's implementation of the reflection API, which is
  * undefined, in general. Rules defined by fields will always be applied
- * before Rules defined by methods.
+ * before Rules defined by methods. You can use a {@link org.junit.rules.RuleChain} if you want
+ * to have control over the order in which the Rules are applied.
  * <p>
  * For example, here is a test class that creates a temporary folder before
  * each test method, and deletes it after each:
