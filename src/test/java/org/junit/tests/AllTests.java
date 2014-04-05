88f7a12d472b1157639db5148b29c8a8825802a3
Stefan Birkner <mail@stefan-birkner.de>
2014-04-06 00:02:03 +0200

    Don't verify the visibility of a method's declaring class.
    I would not expect FrameworkMethod.validatePublicVoid(...) to test
    visibility of the declaring class. Since the last commit the runner
    itself verifies that a test class is public. Hence we can safely remove
    this validation from FrameworkMethod.
    
    This fixes #749 (JUnit cannot be build with JDK 8), too.

--- src/test/java/org/junit/tests/AllTests.java
+++ src/test/java/org/junit/tests/AllTests.java
@@ -99,7 +99,6 @@ import org.junit.tests.running.methods.TestMethodTest;
 import org.junit.tests.running.methods.TimeoutTest;
 import org.junit.tests.validation.BadlyFormedClassesTest;
 import org.junit.tests.validation.FailedConstructionTest;
-import org.junit.tests.validation.InaccessibleBaseClassTest;
 import org.junit.tests.validation.ValidationTest;
 import org.junit.validator.PublicClassValidatorTest;
 
@@ -143,7 +142,6 @@ import org.junit.validator.PublicClassValidatorTest;
         JUnit38ClassRunnerTest.class,
         SystemExitTest.class,
         JUnitCoreReturnsCorrectExitCodeTest.class,
-        InaccessibleBaseClassTest.class,
         SuiteMethodTest.class,
         BadlyFormedClassesTest.class,
         IgnoreClassTest.class,
