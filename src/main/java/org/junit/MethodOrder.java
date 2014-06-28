ccf8c8cf7b8e597e1ebdf0ad903afd0d79d30f85
otereshin <oleg.terjoshin@gmail.com>
2014-06-28 19:06:42 +0400

    Add test for Selected_order sort functionality

--- src/main/java/org/junit/MethodOrder.java
+++ src/main/java/org/junit/MethodOrder.java
@@ -10,7 +10,8 @@ import java.lang.annotation.Target;
 public @interface MethodOrder {
     /**
      * Optionally specify <code>value</code> to have the methods executed in a
-     * particular order
+     * particular order.(add {@link FixMethodOrder} annotation for all test
+     * methods required)
      */
     int value() default 0;
 }
