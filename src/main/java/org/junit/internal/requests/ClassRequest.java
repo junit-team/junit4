ef7b30f2a31e02690c32abb1c412e097726cb8f5
Stefan Birkner <mail@stefan-birkner.de>
2014-07-24 09:38:11 +0200

    Restore field names with f prefix. Fixes #960.
    The prefix has been removed by df00d5eced3a7737b88de0f6f9e3673f0cf88f88,
    because we changed the coding style. Unfortunately IntelliJ IDEA reads
    ClassRequest.fTestClass and FilterRequest.fFilter via reflection. (See
    http://grepcode.com/file/repository.grepcode.com/java/ext/com.jetbrains/intellij-idea/13.0.0/com/intellij/junit4/JUnit4IdeaTestRunner.java/ ) This makes it impossible to
    start tests by using IntelliJ IDEA.
    
    There is already an issue at JetBrains:
    [IDEA-127349 ](http://youtrack.jetbrains.com/issue/IDEA-127349) We can
    revert the fix when this ticket is solved and most users are using an
    IntelliJ version with the fix.

--- src/main/java/org/junit/internal/requests/ClassRequest.java
+++ src/main/java/org/junit/internal/requests/ClassRequest.java
@@ -6,12 +6,18 @@ import org.junit.runner.Runner;
 
 public class ClassRequest extends Request {
     private final Object runnerLock = new Object();
-    private final Class<?> testClass;
+
+    /*
+     * We have to use the f prefix, because IntelliJ's JUnit4IdeaTestRunner uses
+     * reflection to access this field. See
+     * https://github.com/junit-team/junit/issues/960
+     */
+    private final Class<?> fTestClass;
     private final boolean canUseSuiteMethod;
     private volatile Runner runner;
 
     public ClassRequest(Class<?> testClass, boolean canUseSuiteMethod) {
-        this.testClass = testClass;
+        this.fTestClass = testClass;
         this.canUseSuiteMethod = canUseSuiteMethod;
     }
 
@@ -24,7 +30,7 @@ public class ClassRequest extends Request {
         if (runner == null) {
             synchronized (runnerLock) {
                 if (runner == null) {
-                    runner = new AllDefaultPossibilitiesBuilder(canUseSuiteMethod).safeRunnerForClass(testClass);
+                    runner = new AllDefaultPossibilitiesBuilder(canUseSuiteMethod).safeRunnerForClass(fTestClass);
                 }
             }
         }
