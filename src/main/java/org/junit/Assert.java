746499fd82d1e3735bfbb4327baee4f2c8bf1f78
Quincy Bowers <qbowers@clearwateranalytics.com>
2014-09-17 10:42:17 -0600

    Fix for assertEquals(0.0, -0.0, 0.0) and assertEquals(0.0, -0.0) giving different answers.

--- src/main/java/org/junit/Assert.java
+++ src/main/java/org/junit/Assert.java
@@ -108,7 +108,11 @@ public class Assert {
      */
     static public void assertEquals(String message, Object expected,
             Object actual) {
-        if (equalsRegardingNull(expected, actual)) {
+        if (expected instanceof Double && actual instanceof Double) {
+            assertEquals(message, (Double) expected, (Double) actual, 0.0);
+        } else if (expected instanceof Float && actual instanceof Float) {
+            assertEquals(message, (Float) expected, (Float) actual, 0.0);
+        } else if (equalsRegardingNull(expected, actual)) {
             return;
         } else if (expected instanceof String && actual instanceof String) {
             String cleanMessage = message == null ? "" : message;
