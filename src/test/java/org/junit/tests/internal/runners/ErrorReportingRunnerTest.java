24b4748efb175aaaed4d63eae22a8e47e436dfc7
Kevin Cooney <kcooney@google.com>
2015-01-18 20:22:11 -0800

    Fix ErrorReportingRunner behavior to match that for JUnit 4.12 when a test class is passed in.

--- src/test/java/org/junit/tests/internal/runners/ErrorReportingRunnerTest.java
+++ src/test/java/org/junit/tests/internal/runners/ErrorReportingRunnerTest.java
@@ -4,25 +4,14 @@ import org.junit.Test;
 import org.junit.internal.runners.ErrorReportingRunner;
 
 public class ErrorReportingRunnerTest {
-    
+
     @Test(expected = NullPointerException.class)
-    public void cannotCreateWithNullClass() {
-        new ErrorReportingRunner(null, new RuntimeException());
+    public void cannotCreateWithCauseAndNullClass() {
+        new ErrorReportingRunner((Class<?>) null, new RuntimeException());
     }
-    
+
     @Test(expected = NullPointerException.class)
-    public void cannotCreateWithNullClass2() {
-        new ErrorReportingRunner(new RuntimeException(), (Class<?>) null);
+    public void cannotCreateWithCauseAndNullDisplayName() {
+        new ErrorReportingRunner((String) null, new RuntimeException());
     }
-    
-    @Test(expected = NullPointerException.class)
-    public void cannotCreateWithNullClasses() {
-        new ErrorReportingRunner(new RuntimeException(), (Class<?>[]) null);
-    }
-    
-    @Test(expected = NullPointerException.class)
-    public void cannotCreateWithoutClass() {
-        new ErrorReportingRunner(new RuntimeException());
-    }
-    
 }
