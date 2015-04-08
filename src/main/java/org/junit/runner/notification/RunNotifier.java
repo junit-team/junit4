91f0b70e5dc7c9d2aef581007861c5d4b56c0325
Dmitry Baev <charlie@yandex-team.ru>
2015-04-08 13:49:15 +0300

    add suite events to listener

--- src/main/java/org/junit/runner/notification/RunNotifier.java
+++ src/main/java/org/junit/runner/notification/RunNotifier.java
@@ -106,6 +106,38 @@ public class RunNotifier {
     }
 
     /**
+     * Invoke to tell listeners that a test suite is about to start.
+     *
+     * @param description the description of the suite test (generally a class name)
+     * @since 4.13
+     */
+    public void fireTestSuiteStarted(final Description description) {
+        new SafeNotifier() {
+            @Override
+            protected void notifyListener(RunListener each) throws Exception {
+                each.testSuiteStarted(description);
+            }
+        }.run();
+    }
+
+    /**
+     * Invoke to tell listeners that a test suite is about to finish. Always invoke
+     * this method if you invoke {@link #fireTestSuiteStarted(Description)}
+     * as listeners are likely to expect them to come in pairs.
+     *
+     * @param description the description of the suite test (generally a class name)
+     * @since 4.13
+     */
+    public void fireTestSuiteFinished(final Description description) {
+        new SafeNotifier() {
+            @Override
+            protected void notifyListener(RunListener each) throws Exception {
+                each.testSuiteFinished(description);
+            }
+        }.run();
+    }
+
+    /**
      * Invoke to tell listeners that an atomic test is about to start.
      *
      * @param description the description of the atomic test (generally a class and method name)
