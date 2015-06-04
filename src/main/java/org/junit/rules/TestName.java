eedd1f68fbd014beca60c979e8173c271ea5cf2d
Ryan Schmitt <rschmitt@pobox.com>
2015-06-04 10:21:13 -0700

    Upate TestName to make the 'name' field volatile.
    This ensures that the name is published across threads correctly--for
    instance, if a parallelized runner is used.
    
    Closes #1157

--- src/main/java/org/junit/rules/TestName.java
+++ src/main/java/org/junit/rules/TestName.java
@@ -25,7 +25,7 @@ import org.junit.runner.Description;
  * @since 4.7
  */
 public class TestName extends TestWatcher {
-    private String name;
+    private volatile String name;
 
     @Override
     protected void starting(Description d) {
