5607a2445eafc1d648b8dd8727f31ba138ed1f44
Ryan Schmitt <rschmitt@pobox.com>
2015-06-04 08:38:06 -0700

    Wrap line

--- src/main/java/org/junit/Assert.java
+++ src/main/java/org/junit/Assert.java
@@ -1042,7 +1042,8 @@ public class Assert {
             }
         }
         if (message == null) {
-            message = String.format("Expected %s to be thrown, but nothing was thrown", throwableClass.getSimpleName());
+            message = String.format("Expected %s to be thrown, but nothing was thrown",
+                    throwableClass.getSimpleName());
         }
         throw new AssertionError(message);
     }
