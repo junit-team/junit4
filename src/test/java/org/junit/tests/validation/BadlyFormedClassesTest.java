7e7787b2f5d8aba09dd5f635082f3475d86957cc
Alberto Scotto <scotto.alberto.86@gmail.com>
2016-05-11 17:28:13 +0100

    fix unused import

--- src/test/java/org/junit/tests/validation/BadlyFormedClassesTest.java
+++ src/test/java/org/junit/tests/validation/BadlyFormedClassesTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.internal.runners.JUnit4ClassRunner;
