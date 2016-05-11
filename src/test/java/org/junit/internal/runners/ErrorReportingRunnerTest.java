4765762146225f59c1ac802e6d383a52bdedb8de
Alberto Scotto <scotto.alberto.86@gmail.com>
2016-05-11 17:37:45 +0100

    revert unrelated changes

--- src/test/java/org/junit/internal/runners/ErrorReportingRunnerTest.java
+++ src/test/java/org/junit/internal/runners/ErrorReportingRunnerTest.java
@@ -17,22 +17,22 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ErrorReportingRunnerTest {
-
+    
     @Test(expected = NullPointerException.class)
     public void cannotCreateWithNullClass() {
         new ErrorReportingRunner(null, new RuntimeException());
     }
-
+    
     @Test(expected = NullPointerException.class)
     public void cannotCreateWithNullClass2() {
         new ErrorReportingRunner(new RuntimeException(), (Class<?>) null);
     }
-
+    
     @Test(expected = NullPointerException.class)
     public void cannotCreateWithNullClasses() {
         new ErrorReportingRunner(new RuntimeException(), (Class<?>[]) null);
     }
-
+    
     @Test(expected = NullPointerException.class)
     public void cannotCreateWithoutClass() {
         new ErrorReportingRunner(new RuntimeException());
