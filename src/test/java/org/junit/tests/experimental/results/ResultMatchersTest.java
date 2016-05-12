8c23256f467058c4d23ab42d09e132f5785b236d
Alberto Scotto <scotto.alberto.86@gmail.com>
2016-05-12 16:23:42 +0100

    add unit test showing bug

--- src/test/java/org/junit/tests/experimental/results/ResultMatchersTest.java
+++ src/test/java/org/junit/tests/experimental/results/ResultMatchersTest.java
@@ -1,12 +1,16 @@
 package org.junit.tests.experimental.results;
 
+import org.junit.Test;
+import org.junit.experimental.results.PrintableResult;
+import org.junit.experimental.results.ResultMatchers;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.notification.Failure;
+
+import java.util.ArrayList;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-
-import org.junit.Test;
-import org.junit.experimental.results.ResultMatchers;
-import org.junit.experimental.theories.Theory;
 
 public class ResultMatchersTest {
     @Test
@@ -19,5 +23,11 @@ public class ResultMatchersTest {
     public void hasFailuresDescriptionReflectsInput(int i) {
         assertThat(ResultMatchers.failureCountIs(i).toString(),
                 containsString("" + i));
+    }
+
+    @Test
+    public void hasFailureContaining_givenNonMatchingScenario() {
+        PrintableResult resultWithNoFailures = new PrintableResult(new ArrayList<Failure>());
+        assertThat(ResultMatchers.hasFailureContaining("").matches(resultWithNoFailures), is(false));
     }
 }
