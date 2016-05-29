e3ae4cd1e97f5ac85201d7a676d828b015054dc0
Kevin Cooney <kcooney@google.com>
2016-05-29 10:31:15 -0700

    Update Javadoc for fireTestSuiteStarted(), fireTestSuiteFinished(), testSuiteStarted() and testSuiteFinished() to make it clear that not all runners call these methods, but runners that call the started methods should also call the finished methods.
    Closes #1118

--- src/main/java/org/junit/runner/notification/RunNotifier.java
+++ src/main/java/org/junit/runner/notification/RunNotifier.java
@@ -106,7 +106,10 @@ public class RunNotifier {
     }
 
     /**
-     * Invoke to tell listeners that a test suite is about to start.
+     * Invoke to tell listeners that a test suite is about to start. Runners are strongly
+     * encouraged--but not required--to call this method. If this method is called for
+     * a given {@link Description} then {@link #fireTestSuiteFinished(Description)} MUST
+     * be called for the same {@code Description}.
      *
      * @param description the description of the suite test (generally a class name)
      * @since 4.13
