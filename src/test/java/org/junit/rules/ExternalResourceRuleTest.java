586bd1d97e3c0d8365811598aebc91e5e0c1c9da
Alberto Scotto <scotto.alberto.86@gmail.com>
2016-06-25 20:06:25 +0100

    Fix ExternalResource: the test failure was lost..
    .. when the test failed *and* closing the resource failed:
    only the exception coming from the after() method was propagated,
    as per semantics of the try-finally
    (see http://docs.oracle.com/javase/specs/jls/se8/html/jls-14.html#jls-14.20.2).
    
    Finally, the new behavior is compatible with @After method semantics
    (see class RunAfters).
    
    Fixes #1334, point 1.

--- src/test/java/org/junit/rules/ExternalResourceRuleTest.java
+++ src/test/java/org/junit/rules/ExternalResourceRuleTest.java
@@ -1,12 +1,19 @@
 package org.junit.rules;
 
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.junit.experimental.results.PrintableResult.testResult;
 import static org.junit.experimental.results.ResultMatchers.isSuccessful;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
+import org.junit.runner.Description;
+import org.junit.runners.model.MultipleFailureException;
+import org.junit.runners.model.Statement;
 
 public class ExternalResourceRuleTest {
     private static String callSequence;
@@ -40,5 +47,29 @@ public class ExternalResourceRuleTest {
         callSequence = "";
         assertThat(testResult(UsesExternalResource.class), isSuccessful());
         assertEquals("before test after ", callSequence);
+    }
+
+    @Test
+    public void shouldThrowMultipleFailureExceptionWhenTestFailsAndClosingResourceFails() throws Throwable {
+        // given
+        ExternalResource resourceRule = new ExternalResource() {
+            @Override
+            protected void after() {
+                throw new RuntimeException("simulating resource tear down failure");
+            }
+        };
+        Statement failingTest = new Fail(new RuntimeException("simulated test failure"));
+        Description dummyDescription = Description.createTestDescription(
+                "dummy test class name", "dummy test name");
+
+        try {
+            resourceRule.apply(failingTest, dummyDescription).evaluate();
+            fail("ExternalResource should throw");
+        } catch (MultipleFailureException e) {
+            assertThat(e.getMessage(), allOf(
+                    containsString("simulated test failure"),
+                    containsString("simulating resource tear down failure")
+            ));
+        }
     }
 }
