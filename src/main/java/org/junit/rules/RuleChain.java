7e38e390090ded68a7c08eb639dc33a0838f7278
Alberto Scotto <scotto.alberto.86@gmail.com>
2016-07-12 16:43:33 +0100

    RuleChain#around should not allow null args (#1313)
    By throwing IllegalArgEx from around(), we allow for better feedback to users, as the stacktrace will point to the exact line where the null rule is declared.

--- src/main/java/org/junit/rules/RuleChain.java
+++ src/main/java/org/junit/rules/RuleChain.java
@@ -90,10 +90,14 @@ public class RuleChain implements TestRule {
      * Create a new {@code RuleChain}, which encloses the given {@link TestRule} with
      * the rules of the current {@code RuleChain}.
      *
-     * @param enclosedRule the rule to enclose.
+     * @param enclosedRule the rule to enclose; must not be {@code null}.
      * @return a new {@code RuleChain}.
+     * @throws NullPointerException if the argument {@code enclosedRule} is {@code null}
      */
     public RuleChain around(TestRule enclosedRule) {
+        if (enclosedRule == null) {
+            throw new NullPointerException("The enclosed rule must not be null");
+        }
         List<TestRule> rulesOfNewChain = new ArrayList<TestRule>();
         rulesOfNewChain.add(enclosedRule);
         rulesOfNewChain.addAll(rulesStartingWithInnerMost);
