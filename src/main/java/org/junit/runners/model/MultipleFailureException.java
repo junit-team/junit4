70bab9deeda8dd091ccc4d00f163cbd1fd7d40b1
smallyard <liuhongqiang@live.cn>
2016-07-18 10:40:26 +0800

    optimize string format
     In format strings, it is generally preferable better to use %n, which will produce the platform-specific line separator.

--- src/main/java/org/junit/runners/model/MultipleFailureException.java
+++ src/main/java/org/junit/runners/model/MultipleFailureException.java
@@ -34,7 +34,7 @@ public class MultipleFailureException extends Exception {
         StringBuilder sb = new StringBuilder(
                 String.format("There were %d errors:", fErrors.size()));
         for (Throwable e : fErrors) {
-            sb.append(String.format("\n  %s(%s)", e.getClass().getName(), e.getMessage()));
+            sb.append(String.format("%n  %s(%s)", e.getClass().getName(), e.getMessage()));
         }
         return sb.toString();
     }
