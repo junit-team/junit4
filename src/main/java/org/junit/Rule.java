ee4403455a9573a67668da695fea223f77606326
Jimmy Shi <Jimmy-Shi@users.noreply.github.com>
2016-10-03 22:09:20 +0800

    Improve confusing Javadoc about the Rule running order (#1149)

--- src/main/java/org/junit/Rule.java
+++ src/main/java/org/junit/Rule.java
@@ -16,12 +16,14 @@ import java.lang.annotation.Target;
  * to the {@link org.junit.rules.TestRule} will run any {@link Before} methods,
  * then the {@link Test} method, and finally any {@link After} methods,
  * throwing an exception if any of these fail.  If there are multiple
- * annotated {@link Rule}s on a class, they will be applied in order of fields first, then methods.
+ * annotated {@link Rule}s on a class, they will be applied in order of methods first, then fields.
  * However, if there are multiple fields (or methods) they will be applied in an order
  * that depends on your JVM's implementation of the reflection API, which is
  * undefined, in general. Rules defined by fields will always be applied
- * before Rules defined by methods. You can use a {@link org.junit.rules.RuleChain} if you want
- * to have control over the order in which the Rules are applied.
+ * after Rules defined by methods, i.e. the Statements returned by the former will
+ * be executed around those returned by the latter. You can use a {@link org.junit.rules.RuleChain} 
+ * if you want to have control over the order in which the Rules are applied.
+ *
  * <p>
  * For example, here is a test class that creates a temporary folder before
  * each test method, and deletes it after each:
