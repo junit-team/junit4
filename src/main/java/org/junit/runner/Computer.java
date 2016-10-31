23b0bb0ef9b690660cd3853005f9b635533d3770
Peter Wippermann <PeterWippermann@users.noreply.github.com>
2016-10-31 16:39:34 +0100

    Test for #1320 - Description produced by Request.classes() shouldn't be null (#1377)
    Fixes #1320

--- src/main/java/org/junit/runner/Computer.java
+++ src/main/java/org/junit/runner/Computer.java
@@ -30,7 +30,17 @@ public class Computer {
             public Runner runnerForClass(Class<?> testClass) throws Throwable {
                 return getRunner(builder, testClass);
             }
-        }, classes);
+        }, classes) {
+            @Override
+            protected String getName() {
+                /*
+                 * #1320 The generated suite is not based on a real class so
+                 * only a 'null' description can be generated from it. This name
+                 * will be overridden here.
+                 */
+                return "classes";
+            }
+        };
     }
 
     /**
