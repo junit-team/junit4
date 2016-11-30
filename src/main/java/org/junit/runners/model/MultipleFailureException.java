638e82435a01af83e77c23e9f8d7b658a89eb4bf
smallyard <liuhongqiang@live.cn>
2016-11-30 12:56:54 +0800

    Use %n in string.format() (#1346)
    In format strings, it is generally preferable better to use %n, which will produce the platform-specific line separator.

--- src/main/java/org/junit/runners/model/MultipleFailureException.java
+++ src/main/java/org/junit/runners/model/MultipleFailureException.java
@@ -40,7 +40,7 @@ public class MultipleFailureException extends Exception {
         StringBuilder sb = new StringBuilder(
                 String.format("There were %d errors:", fErrors.size()));
         for (Throwable e : fErrors) {
-            sb.append(String.format("\n  %s(%s)", e.getClass().getName(), e.getMessage()));
+            sb.append(String.format("%n  %s(%s)", e.getClass().getName(), e.getMessage()));
         }
         return sb.toString();
     }
