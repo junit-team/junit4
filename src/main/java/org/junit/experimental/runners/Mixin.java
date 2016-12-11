1a4e0e8c9c6602908944d47cdf0bd78d44ef3a24
Ollie <ollie.robertshaw@gmail.com>
2016-12-11 09:33:17 +0000

    Tidy

--- src/main/java/org/junit/experimental/runners/Mixin.java
+++ src/main/java/org/junit/experimental/runners/Mixin.java
@@ -2,7 +2,6 @@ package org.junit.experimental.runners;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
