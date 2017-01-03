59074ee2341019bb3f2e1f21dc92ada2a2906ef4
Kevin Cooney <kcooney@google.com>
2017-01-02 20:27:44 -0800

    Update Classes.getClass() to never use the bootstrap class loader. Previously, if there was no context class loader then the bootstrap class loader is used. Now, the class loader for Classes is used (or for the passed in class, if the new overloaded version of getClass() is used).

--- src/main/java/org/junit/internal/Classes.java
+++ src/main/java/org/junit/internal/Classes.java
@@ -17,11 +17,28 @@ public class Classes {
 
     /**
      * Returns Class.forName for {@code className} using the current thread's class loader.
+     * If the current thread does not have a class loader, falls back to the class loader for
+     * {@link Classes}.
      *
      * @param className Name of the class.
      * @throws ClassNotFoundException
      */
     public static Class<?> getClass(String className) throws ClassNotFoundException {
-        return Class.forName(className, true, currentThread().getContextClassLoader());
+        return getClass(className, Classes.class);
+    }
+
+    /**
+     * Returns Class.forName for {@code className} using the current thread's class loader.
+     * If the current thread does not have a class loader, falls back to the class loader for the
+     * passed-in class.
+     *
+     * @param className Name of the class.
+     * @param callingClass Class that is requesting a the class
+     * @throws ClassNotFoundException
+     * @since 4.13
+     */
+    public static Class<?> getClass(String className, Class<?> callingClass) throws ClassNotFoundException {
+        ClassLoader classLoader = currentThread().getContextClassLoader();
+        return Class.forName(className, true, classLoader == null ? callingClass.getClassLoader() : classLoader);
     }
 }
