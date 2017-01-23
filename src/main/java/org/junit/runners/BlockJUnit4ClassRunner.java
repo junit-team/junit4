junit-team/junit4

commit 1d97da73fdafce8f5cf9d722f23c5cba41b26b70

Verify that test classes are public.
Test classes should be public in order to avoid problems with
reflection caused by the security manager. Public classes are always
accesible.

src/main/java/org/junit/runners/ParentRunner.java
@@ -6,6 +6,7 @@ import static org.junit.internal.runners.rules.RuleFieldValidator.CLASS_RULE_VAL
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -38,6 +39,8 @@ import org.junit.runners.model.RunnerScheduler;
 import org.junit.runners.model.Statement;
 import org.junit.runners.model.TestClass;
 import org.junit.validator.AnnotationsValidator;
+import org.junit.validator.PublicClassValidator;
+import org.junit.validator.TestClassValidator;
 
 /**
  * Provides most of the functionality specific to a Runner that implements a
@@ -54,13 +57,14 @@ import org.junit.validator.AnnotationsValidator;
  */
 public abstract class ParentRunner<T> extends Runner implements Filterable,
         Sortable {
+    private static final List<TestClassValidator> VALIDATORS = Arrays.asList(
+            new AnnotationsValidator(), new PublicClassValidator());
+
     private final Object fChildrenLock = new Object();
     private final TestClass fTestClass;
 
     // Guarded by fChildrenLock
     private volatile Collection<T> fFilteredChildren = null;
-
-    private final AnnotationsValidator fAnnotationsValidator= new AnnotationsValidator();
 
     private volatile RunnerScheduler fScheduler = new RunnerScheduler() {
         public void schedule(Runnable childStatement) {
@@ -121,7 +125,15 @@ public abstract class ParentRunner<T> extends Runner implements Filterable,
         validatePublicVoidNoArgMethods(BeforeClass.class, true, errors);
         validatePublicVoidNoArgMethods(AfterClass.class, true, errors);
         validateClassRules(errors);
-        errors.addAll(fAnnotationsValidator.validateTestClass(getTestClass()));
+        applyValidators(errors);
+    }
+
+    private void applyValidators(List<Throwable> errors) {
+        if (getTestClass().getJavaClass() != null) {
+            for (TestClassValidator each : VALIDATORS) {
+                errors.addAll(each.validateTestClass(getTestClass()));
+            }
+        }
     }
 
     /**

src/test/java/org/junit/tests/running/classes/ParentRunnerTest.java
@@ -84,7 +84,7 @@ public class ParentRunnerTest {
     }
 
     private static class Exclude extends Filter {
-        private String methodName;
+        private final String methodName;
 
         public Exclude(String methodName) {
             this.methodName = methodName;
@@ -125,6 +125,18 @@ public class ParentRunnerTest {
     public void failWithHelpfulMessageForNonStaticClassRule() {
         assertClassHasFailureMessage(TestWithNonStaticClassRule.class,
                 "The @ClassRule 'temporaryFolder' must be static.");
+    }
+
+    static class NonPublicTestClass {
+        public NonPublicTestClass() {
+        }
+    }
+
+    @Test
+    public void cannotBeCreatedWithNonPublicTestClass() {
+        assertClassHasFailureMessage(
+                NonPublicTestClass.class,
+                "The class org.junit.tests.running.classes.ParentRunnerTest$NonPublicTestClass is not public.");
     }
 
     private void assertClassHasFailureMessage(Class<?> klass, String message) {

src/main/java/org/junit/runners/model/TestClass.java
@@ -7,6 +7,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -246,6 +247,10 @@ public class TestClass implements Annotatable {
             }
         }
         return results;
+    }
+
+    public boolean isPublic() {
+        return Modifier.isPublic(fClass.getModifiers());
     }
 
     public boolean isANonStaticInnerClass() {

src/main/java/org/junit/validator/AnnotationsValidator.java
@@ -18,7 +18,7 @@ import org.junit.runners.model.TestClass;
  * 
  * @since 4.12
  */
-public final class AnnotationsValidator {
+public final class AnnotationsValidator implements TestClassValidator {
     private static final List<AnnotatableValidator<?>> VALIDATORS = Arrays.<AnnotatableValidator<?>>asList(
             new ClassValidator(), new MethodValidator(), new FieldValidator());
 

src/main/java/org/junit/validator/PublicClassValidator.java
@@ -1,0 +1,33 @@
+package org.junit.validator;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
+import java.util.List;
+
+import org.junit.runners.model.TestClass;
+
+/**
+ * Validates that a {@link TestClass} is public.
+ * 
+ * @since 4.12
+ */
+public class PublicClassValidator implements TestClassValidator {
+    private static final List<Exception> NO_VALIDATION_ERRORS = emptyList();
+
+    /**
+     * Validate that the specified {@link TestClass} is public.
+     * 
+     * @param testClass the {@link TestClass} that is validated.
+     * @return an empty list if the class is public or a list with a single
+     *         exception otherwise.
+     */
+    public List<Exception> validateTestClass(TestClass testClass) {
+        if (testClass.isPublic()) {
+            return NO_VALIDATION_ERRORS;
+        } else {
+            return singletonList(new Exception("The class "
+                    + testClass.getName() + " is not public."));
+        }
+    }
+}

src/main/java/org/junit/validator/TestClassValidator.java
@@ -1,0 +1,21 @@
+package org.junit.validator;
+
+import java.util.List;
+
+import org.junit.runners.model.TestClass;
+
+/**
+ * Validates a single facet of a test class.
+ * 
+ * @since 4.12
+ */
+public interface TestClassValidator {
+    /**
+     * Validate a single facet of a test class.
+     * 
+     * @param testClass
+     *            the {@link TestClass} that is validated.
+     * @return the validation errors found by the validator.
+     */
+    public List<Exception> validateTestClass(TestClass testClass);
+}

src/test/java/org/junit/runners/model/TestClassTest.java
@@ -201,4 +201,24 @@ public class TestClassTest {
         testClass.hashCode();
         // everything is fine if no exception is thrown.
     }
+
+    public static class PublicClass {
+
+    }
+
+    @Test
+    public void identifiesPublicModifier() {
+        TestClass tc = new TestClass(PublicClass.class);
+        assertEquals("Wrong flag 'public',", true, tc.isPublic());
+    }
+
+    static class NonPublicClass {
+
+    }
+    
+    @Test
+    public void identifiesNonPublicModifier() {
+        TestClass tc = new TestClass(NonPublicClass.class);
+        assertEquals("Wrong flag 'public',", false, tc.isPublic());
+    }
 }

src/test/java/org/junit/tests/AllTests.java
@@ -101,6 +101,7 @@ import org.junit.tests.validation.BadlyFormedClassesTest;
 import org.junit.tests.validation.FailedConstructionTest;
 import org.junit.tests.validation.InaccessibleBaseClassTest;
 import org.junit.tests.validation.ValidationTest;
+import org.junit.validator.PublicClassValidatorTest;
 
 // These test files need to be cleaned. See
 // https://sourceforge.net/pm/task.php?func=detailtask&project_task_id=136507&group_id=15278&group_project_id=51407
@@ -204,7 +205,8 @@ import org.junit.tests.validation.ValidationTest;
         FrameworkMethodTest.class,
         FailOnTimeoutTest.class,
         JUnitCoreTest.class,
-        TestWithParametersTest.class
+        TestWithParametersTest.class,
+        PublicClassValidatorTest.class
 })
 public class AllTests {
     public static Test suite() {

commit 1faa5521a3750dd1798aa010507607924e21cf88

Classes annotated with @RunWith(Suite.class) do not need to be public.
This fixes a regression in JUnit 4.12 introduced by
https://github.com/junit-team/junit4/commit/1d97da7.

src/main/java/org/junit/runners/BlockJUnit4ClassRunner.java
@@ -30,6 +30,8 @@ import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.MultipleFailureException;
 import org.junit.runners.model.Statement;
+import org.junit.validator.PublicClassValidator;
+import org.junit.validator.TestClassValidator;
 
 /**
  * Implements the JUnit 4 standard test case class model, as defined by the
@@ -56,6 +58,7 @@ import org.junit.runners.model.Statement;
  * @since 4.5
  */
 public class BlockJUnit4ClassRunner extends ParentRunner<FrameworkMethod> {
+    private static TestClassValidator PUBLIC_CLASS_VALIDATOR = new PublicClassValidator();
 
     private final ConcurrentMap<FrameworkMethod, Description> methodDescriptions = new ConcurrentHashMap<FrameworkMethod, Description>();
 
@@ -133,11 +136,18 @@ public class BlockJUnit4ClassRunner extends ParentRunner<FrameworkMethod> {
     protected void collectInitializationErrors(List<Throwable> errors) {
         super.collectInitializationErrors(errors);
 
+        validatePublicConstructor(errors);
         validateNoNonStaticInnerClass(errors);
         validateConstructor(errors);
         validateInstanceMethods(errors);
         validateFields(errors);
         validateMethods(errors);
+    }
+
+    private void validatePublicConstructor(List<Throwable> errors) {
+        if (getTestClass().getJavaClass() != null) {
+            errors.addAll(PUBLIC_CLASS_VALIDATOR.validateTestClass(getTestClass()));
+        }
     }
 
     protected void validateNoNonStaticInnerClass(List<Throwable> errors) {

src/main/java/org/junit/runners/ParentRunner.java
@@ -40,7 +40,6 @@ import org.junit.runners.model.RunnerScheduler;
 import org.junit.runners.model.Statement;
 import org.junit.runners.model.TestClass;
 import org.junit.validator.AnnotationsValidator;
-import org.junit.validator.PublicClassValidator;
 import org.junit.validator.TestClassValidator;
 
 /**
@@ -58,8 +57,8 @@ import org.junit.validator.TestClassValidator;
  */
 public abstract class ParentRunner<T> extends Runner implements Filterable,
         Sortable {
-    private static final List<TestClassValidator> VALIDATORS = Arrays.asList(
-            new AnnotationsValidator(), new PublicClassValidator());
+    private static final List<TestClassValidator> VALIDATORS = Arrays.<TestClassValidator>asList(
+            new AnnotationsValidator());
 
     private final Object childrenLock = new Object();
     private final TestClass testClass;

src/test/java/org/junit/tests/running/classes/ParentRunnerTest.java
@@ -1,7 +1,6 @@
 package org.junit.tests.running.classes;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
@@ -137,6 +136,9 @@ public class ParentRunnerTest {
     static class NonPublicTestClass {
         public NonPublicTestClass() {
         }
+
+        @Test
+        public void alwaysPasses() {}
     }
 
     @Test

src/test/java/org/junit/tests/running/classes/SuiteTest.java
@@ -1,5 +1,6 @@
 package org.junit.tests.running.classes;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -41,6 +42,18 @@ public class SuiteTest {
     public static class All {
     }
 
+    @RunWith(Suite.class)
+    @SuiteClasses(TestA.class)
+    static class NonPublicSuite {
+    }
+
+    @RunWith(Suite.class)
+    @SuiteClasses(TestA.class)
+    static class NonPublicSuiteWithBeforeClass {
+        @BeforeClass
+        public static void doesNothing() {}
+    }
+
     public static class InheritsAll extends All {
     }
 
@@ -64,6 +77,19 @@ public class SuiteTest {
     public void suiteTestCountIsCorrect() throws Exception {
         Runner runner = Request.aClass(All.class).getRunner();
         assertEquals(2, runner.testCount());
+    }
+
+    @Test
+    public void suiteClassDoesNotNeedToBePublic() {
+        JUnitCore core = new JUnitCore();
+        Result result = core.run(NonPublicSuite.class);
+        assertEquals(1, result.getRunCount());
+        assertEquals(0, result.getFailureCount());
+    }
+
+    @Test
+    public void nonPublicSuiteClassWithBeforeClassFailsWithoutRunningTests() {
+        assertThat(testResult(NonPublicSuiteWithBeforeClass.class), hasSingleFailureContaining("can not access"));
     }
 
     @Test