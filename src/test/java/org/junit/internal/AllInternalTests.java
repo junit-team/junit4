a29f45a3be64caa53feee6f6cc4d70cc43542362
Alberto Scotto <scotto.alberto.86@gmail.com>
2017-02-14 03:18:08 +0100

    Tests expecting AssumptionViolatedException should be marked as passed, not skipped (#1291)
    Tests annotated with `@Test(expected = AssumptionViolatedException.class)`
    which throw AssumptionViolatedException should be marked as passing, not skipped.
    
    Fixes #1290

--- src/test/java/org/junit/internal/AllInternalTests.java
+++ src/test/java/org/junit/internal/AllInternalTests.java
@@ -4,6 +4,7 @@ import org.junit.internal.builders.AnnotatedBuilderTest;
 import org.junit.internal.matchers.StacktracePrintingMatcherTest;
 import org.junit.internal.matchers.ThrowableCauseMatcherTest;
 import org.junit.internal.runners.ErrorReportingRunnerTest;
+import org.junit.internal.runners.statements.ExpectExceptionTest;
 import org.junit.internal.runners.statements.FailOnTimeoutTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -13,6 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
         AnnotatedBuilderTest.class,
         ErrorReportingRunnerTest.class,
+        ExpectExceptionTest.class,
         FailOnTimeoutTest.class,
         MethodSorterTest.class,
         StacktracePrintingMatcherTest.class,
