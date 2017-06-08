14a1520e5d4931e2405e2074466182919a3b5e0e
Alex Panchenko <alex.panchenko@gmail.com>
2017-06-08 08:10:55 +0700

    test added

--- src/test/java/org/junit/tests/running/classes/BlockJUnit4ClassRunnerTest.java
+++ src/test/java/org/junit/tests/running/classes/BlockJUnit4ClassRunnerTest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.notification.RunListener;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
@@ -29,5 +32,37 @@ public class BlockJUnit4ClassRunnerTest {
                     "The inner class org.junit.tests.running.classes.BlockJUnit4ClassRunnerTest$OuterClass$Enclosed is not static.",
                     causes.get(0).getMessage());
         }
+    }
+
+    private static String log;
+
+    public static class MethodBlockAfterFireTestStarted {
+        public MethodBlockAfterFireTestStarted() {
+            log += " init";
+        }
+
+        @Test
+        public void test() {
+            log += " test";
+        }
+    }
+
+    @Test
+    public void methodBlockAfterFireTestStarted() {
+        log = "";
+        JUnitCore junit = new JUnitCore();
+        junit.addListener(new RunListener() {
+            @Override
+            public void testStarted(Description description) throws Exception {
+                log += " testStarted(" + description.getMethodName() + ")";
+            }
+
+            @Override
+            public void testFinished(Description description) throws Exception {
+                log += " testFinished(" + description.getMethodName() + ")";
+            }
+        });
+        junit.run(MethodBlockAfterFireTestStarted.class);
+        assertEquals(" testStarted(test) init test testFinished(test)", log);
     }
 }
