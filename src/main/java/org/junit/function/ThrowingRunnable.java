f1972d7ab1681618f51a1ad6648520e0ec8f02ca
Alex Panchenko <alex.panchenko@gmail.com>
2018-02-15 13:26:25 +0700

    rename expectThrows() to assertThrows()

--- src/main/java/org/junit/function/ThrowingRunnable.java
+++ src/main/java/org/junit/function/ThrowingRunnable.java
@@ -1,8 +1,10 @@
 package org.junit.function;
 
 /**
- * This interface facilitates the use of expectThrows from Java 8. It allows method references
- * to void methods (that declare checked exceptions) to be passed directly into expectThrows
+ * This interface facilitates the use of
+ * {@link org.junit.Assert#assertThrows(Class, ThrowingRunnable)} from Java 8. It allows method
+ * references to void methods (that declare checked exceptions) to be passed directly into
+ * {@code assertThrows}
  * without wrapping. It is not meant to be implemented directly.
  *
  * @since 4.13
