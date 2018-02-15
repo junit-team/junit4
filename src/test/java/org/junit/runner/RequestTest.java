697764155585743dc7e03df3c4451e3f52615c1b
Stefan Birkner <mail@stefan-birkner.de>
2018-02-15 11:54:16 +0100

    Add test for handling InitializationError
    The code was changed by commit 0804ef4cd09b0e94783a9882c60914e22fca5108
    but unfortunately the test was not part of the commit. The test is based
    on Philip Graf's test in pull request #1065.
    
    Co-authored-by: Philip Graf <git@acanda.ch>

--- src/test/java/org/junit/runner/RequestTest.java
+++ src/test/java/org/junit/runner/RequestTest.java
@@ -2,8 +2,12 @@ package org.junit.runner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.rules.EventCollector.hasSingleFailureWithMessage;
 
 import org.junit.Test;
+import org.junit.rules.EventCollector;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.RunnerBuilder;
 
 public class RequestTest {
 
@@ -17,5 +21,30 @@ public class RequestTest {
                 .classes(RequestTest.class, RequestTest.class).getRunner()
                 .getDescription();
         assertThat(description.toString(), is("classes"));
+    }
+
+    @Test
+    public void reportsInitializationErrorThrownWhileCreatingSuite() {
+        EventCollector collector = new EventCollector();
+        JUnitCore core = new JUnitCore();
+        core.addListener(collector);
+
+        core.run(new FailingComputer(), FooTest.class, BarTest.class);
+
+        assertThat(collector, hasSingleFailureWithMessage("cannot create suite"));
+    }
+
+    private static class FailingComputer extends Computer {
+        @Override
+        public Runner getSuite(RunnerBuilder builder, Class<?>[] classes)
+                throws InitializationError {
+            throw new InitializationError("cannot create suite");
+        }
+    }
+
+    private static class FooTest {
+    }
+
+    private static class BarTest {
     }
 }
