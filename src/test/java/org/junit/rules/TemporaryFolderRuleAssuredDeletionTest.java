dc988e2447a44588baec811259d0f36ca9e2995e
Stefan Birkner <mail@stefan-birkner.de>
2018-10-02 21:33:05 +0200

    Use standard TemporaryFolder for test
    KISS

--- src/test/java/org/junit/rules/TemporaryFolderRuleAssuredDeletionTest.java
+++ src/test/java/org/junit/rules/TemporaryFolderRuleAssuredDeletionTest.java
@@ -6,72 +6,40 @@ import static org.junit.experimental.results.PrintableResult.testResult;
 import static org.junit.experimental.results.ResultMatchers.failureCountIs;
 import static org.junit.experimental.results.ResultMatchers.isSuccessful;
 
-import java.io.IOException;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.results.PrintableResult;
 
 public class TemporaryFolderRuleAssuredDeletionTest {
 
-    private static class StubTemporaryFolder extends TemporaryFolder {
-        private StubTemporaryFolder(Builder builder) {
-            super(builder);
-        }
+    public static class TestClass {
+        static TemporaryFolder injectedRule;
 
-        /**
-         * Don't need to create as we are overriding deletion.
-         */
-        @Override
-        public void create() throws IOException {
-        }
-
-        /**
-         * Simulates failure to clean-up temporary folder.
-         */
-        @Override
-        protected boolean tryDelete() {
-            return false;
-        }
-
-        public static Builder builder() {
-            return new TemporaryFolder.Builder() {
-                @Override
-                public TemporaryFolder build() {
-                    return new StubTemporaryFolder(this);
-               }
-            };
-        }
-    }
-
-    public static class HasTempFolderWithAssuredDeletion {
-        @Rule public TemporaryFolder folder = StubTemporaryFolder.builder()
-                .assureDeletion()
-                .build();
+        @Rule
+        public TemporaryFolder folder = injectedRule;
 
         @Test
-        public void alwaysPasses() {
+        public void alwaysPassesButDeletesRootFolder() {
+            //we delete the folder in the test so that it cannot be deleted by
+            //the rule
+            folder.getRoot().delete();
         }
     }
 
     @Test
-    public void testStrictVerificationFailure() {
-        PrintableResult result = testResult(HasTempFolderWithAssuredDeletion.class);
+    public void testFailsWhenCreatedFolderCannotBeDeletedButDeletionIsAssured() {
+        TestClass.injectedRule = TemporaryFolder.builder()
+                .assureDeletion()
+                .build();
+        PrintableResult result = testResult(TestClass.class);
         assertThat(result, failureCountIs(1));
         assertThat(result.toString(), containsString("Unable to clean up temporary folder"));
     }
 
-    public static class HasTempFolderWithoutAssuredDeletion {
-        @Rule public TemporaryFolder folder = StubTemporaryFolder.builder().build();
-
-        @Test
-        public void alwaysPasses() {
-        }
-    }
-
     @Test
-    public void testStrictVerificationSuccess() {
-        PrintableResult result = testResult(HasTempFolderWithoutAssuredDeletion.class);
+    public void byDefaultTestDoesNotFailWhenCreatedFolderCannotBeDeleted() {
+        TestClass.injectedRule = new TemporaryFolder();
+        PrintableResult result = testResult(TestClass.class);
         assertThat(result, isSuccessful());
     }
 }
