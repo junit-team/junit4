c7c62448f2e3194f1fa32f924e0c1a395e45f8bd
Leonard Broman <leonard.broman@dewire.com>
2018-10-11 16:10:53 +0200

    Fixed small incorrect javadoc link

--- src/main/java/org/junit/runners/model/MemberValueConsumer.java
+++ src/main/java/org/junit/runners/model/MemberValueConsumer.java
@@ -11,7 +11,7 @@ public interface MemberValueConsumer<T> {
     /**
      * Receives the next value and its declaring member.
      *
-     * @param member declaring member ({@link FrameworkMethod or {@link FrameworkField}}
+     * @param member declaring member ({@link FrameworkMethod} or {@link FrameworkField})
      * @param value the value of the next member
      */
     void accept(FrameworkMember member, T value);
