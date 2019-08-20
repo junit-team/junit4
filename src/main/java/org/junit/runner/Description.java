7e79656b97c598190c435af5c321bd9c9d9f887c
Michael Ernst <mernst@cs.washington.edu>
2019-08-19 21:51:17 -0700

    Add a few annotations

--- src/main/java/org/junit/runner/Description.java
+++ src/main/java/org/junit/runner/Description.java
@@ -156,13 +156,13 @@ public class Description implements Serializable {
     private final String fDisplayName;
     private final Serializable fUniqueId;
     private final Annotation[] fAnnotations;
-    private volatile /* write-once */ Class<?> fTestClass;
+    private volatile /* write-once */ @MonotonicNonNull Class<?> fTestClass;
 
-    private Description(Class<?> clazz, String displayName, Annotation... annotations) {
+    private Description(@Nullable Class<?> clazz, String displayName, Annotation... annotations) {
         this(clazz, displayName, displayName, annotations);
     }
 
-    private Description(Class<?> testClass, String displayName, Serializable uniqueId, Annotation... annotations) {
+    private Description(@Nullable Class<?> testClass, String displayName, Serializable uniqueId, Annotation... annotations) {
         if ((displayName == null) || (displayName.length() == 0)) {
             throw new IllegalArgumentException(
                     "The display name must not be empty.");
